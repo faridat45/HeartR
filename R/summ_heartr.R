29ca9a38b42b74179e03fecd839ed05f9a4a0995
ciaraolo7 <ciara.olohan.2023@mumail.ie>
2026-01-02 09:51:55 +0000

    fixed error

--- R/summ_heartr.R
+++ R/summ_heartr.R
@@ -15,7 +15,7 @@
 #' @author Ciara Olohan - <\email{ciara.olohan.2023@@mumail.ie}>
 #' @importFrom stats "quantile" "median" "sd" "var"
 #' @examples
-#' weight_height_bmisumm <- summ_heartr(heart_dat$Weight, heart_dat$Height, heart_dat$BMI)
+#' summ_heartr(heart_dat$Weight, heart_dat$Height, heart_dat$BMI)
 #'
 #'
 #'
@@ -37,7 +37,7 @@ summ_heartr <- function(...){
       Minimum = min(x),
       Q1 = stats::quantile(x, 0.25),
       Median = stats::median(x),
-      Q3 = stats::quantile(x),
+      Q3 = stats::quantile(x, 0.75),
       Maximum = max(x),
       SD = stats::sd(x),
       Var = stats::var(x)
@@ -46,7 +46,7 @@ summ_heartr <- function(...){
 
   if(length(input) == 1) {
     finalsumm <- summ(input[[1]], newnames[1])
-    class(finalsum) <- "summ_heartr"
+    class(finalsumm) <- "summ_heartr"
     return(finalsumm)
   }
 
@@ -54,7 +54,10 @@ summ_heartr <- function(...){
     list_output <- mapply(summ, input, name = newnames, SIMPLIFY = FALSE)
 
     finalsumm <- do.call(rbind, list_output)
-    class(finalsumm <- "summ_heartr")
+    class(finalsumm) <- "summ_heartr"
     return(finalsumm)
   }
 }
+
+summ_heartr(heart_dat$Age)
+
